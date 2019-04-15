b2058842131300cc05156d5fc6a309cb5886e313
simajune <tejayjung@gmail.com>
2019-04-15 09:11:21 +0900

    - 날씨 앱 mvvm으로 만들기

---
+++ GoodWeather/GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Mohammad Azam on 12/1/18.
+//  Copyright © 2018 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherCell
+        
+        cell.cityNameLabel.text = "Houston"
+        cell.temperatureLabel.text = "70°"
+        return cell
+    }
+    
+}
---
+++ GoodWeather/GoodWeather/ViewModels/WeatherListViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  WeatherListViewModel.swift
+//  GoodWeather
+//
+//  Created by Mohammad Azam on 12/2/18.
+//  Copyright © 2018 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherListViewModel {
+    
+    private var weatherViewModels = [WeatherViewModel]()
+    
+}
+
+struct WeatherViewModel: Decodable {
+    
+    let name: String
+    let currentTemperature: TemperatureViewModel
+    
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case currentTemperature = "main"
+    }
+    
+}
+
+struct TemperatureViewModel: Decodable {
+    
+    let temperature: Double
+    let temperatureMin: Double
+    let temperatureMax: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case temperature = "temp"
+        case temperatureMin = "temp_min"
+        case temperatureMax = "temp_max"
+    }
+}
