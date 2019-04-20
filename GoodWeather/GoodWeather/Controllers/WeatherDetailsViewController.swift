1f4352d69dc3c402c79843d64b5910af79c83b3f
simajune <tejayjung@gmail.com>
2019-04-21 01:21:31 +0900

    - Complete lesson 35

---
+++ GoodWeather/GoodWeather/Controllers/WeatherDetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDetailsViewController.swift
+//  GoodWeather
+//
+//  Created by SIMA on 21/04/2019.
+//  Copyright © 2019 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
