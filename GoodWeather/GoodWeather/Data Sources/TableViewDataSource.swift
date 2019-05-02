cc58dce631166a04104a27961c65f4eff9376097
simajune <tejayjung@gmail.com>
2019-05-02 23:53:01 +0900

    - Complete lesson 43

---
+++ GoodWeather/GoodWeather/Data Sources/TableViewDataSource.swift
@@ -0,0 +1,37 @@
+//
+//  TableViewDataSource.swift
+//  GoodWeather
+//
+//  Created by SIMA on 02/05/2019.
+//  Copyright © 2019 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewDataSource<CellType, ViewModel>: NSObject, UITableViewDataSource where CellType: UITableViewCell {
+    
+    
+    let cellIdetifier: String
+    var items: [ViewModel]
+    let configureCell: (CellType, ViewModel) -> ()
+    
+    init(cellIdetifier: String, items: [ViewModel], configureCell: @escaping (CellType, ViewModel) -> () ) {
+        self.cellIdetifier = cellIdetifier
+        self.items = items
+        self.configureCell = configureCell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdetifier, for: indexPath) as? CellType else{
+            fatalError("Cell with idetifier \(self.cellIdetifier) not found")
+        }
+        let vm = self.items[indexPath.row]
+        self.configureCell(cell,vm)
+        return cell
+    }
+}
