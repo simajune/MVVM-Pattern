d7fcb6f1d3b5af7fb7c86548138a46ca504aa6a3
simajune <tejayjung@gmail.com>
2019-05-30 23:42:25 +0900

    - Complete Lesson 77 ~	79

---
+++ GoodWeather/GoodWeatherTests/WeatherListViewModelTests.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherListViewModelTests.swift
+//  GoodWeatherTests
+//
+//  Created by SIMA on 30/05/2019.
+//  Copyright © 2019 Mohammad Azam. All rights reserved.
+//
+
+import XCTest
+@testable import GoodWeather
+
+class WeatherListViewModelTests: XCTestCase {
+
+    private var weatherListVM: WeatherListViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        
+        
+        
+//        self.weatherListVM.addWeatherViewModel(WeatherViewModel(name: "Houtson", currentTemperature: TemperatureViewModel(temperature: 32, temperatureMin: 0, temperatureMax: 0)))
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+   
+
+}
