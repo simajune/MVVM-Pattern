3b88e1383fc59b145dc302a20e9109cbae26657b
simajune <tejayjung@gmail.com>
2019-06-06 22:46:13 +0900

    - Complete Lesson 11

---
+++ GoodNews/GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by SIMA on 06/06/2019.
+//  Copyright © 2019 TJ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
